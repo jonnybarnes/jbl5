65fa642dd686c5f731df86224c5068d4044de2bf
Jonny Barnes <jonny@jonnybarnes.uk>
2015-08-15 18:56:23 +0100

    Drop piwik for now

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -60,22 +60,5 @@
 
   return t;
 }(document, "script", "twitter-wjs"));</script>
-<?php if (App::environment('production')) { ?>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.jonnybarnes.uk/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="https://piwik.jonnybarnes.uk/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
-<?php } ?>
 </body>
 </html>
