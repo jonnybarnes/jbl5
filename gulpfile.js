0c121e088d42c1c24579978530c9f1b9db55689a
Jonny Barnes <jonny@jonnybarnes.uk>
2015-08-21 21:01:46 +0100

    Gzip static content
    Create `file.gz` with max compression of static files
    for use with nginx’s `gzip_static` module.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-ruby-sass');
 var prefix = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var gzip = require('gulp-gzip');
 
 gulp.task('sass', function() {
 	return gulp.src('scss/global.scss')
@@ -11,10 +12,41 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('public/assets/css'))
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('process-sass', ['sass'], function() {
 	return gulp.src('public/assets/css/global.css')
 		.pipe(prefix('last 2 versions'))
 		.pipe(minifycss({processImport:false}))
 		.pipe(rename('style.css'))
 		.pipe(gulp.dest('public/assets/css'))
 });
+
+gulp.task('gzip-css', ['process-sass'], function() {
+	return gulp.src('public/assets/css/*.css')
+		.pipe(gzip({ gzipOptions: { level: 9 }, append: true }))
+		.pipe(gulp.dest('public/assets/css'))
+});
+
+gulp.task('gzip-js', function() {
+	gulp.src('public/assets/js/*.js')
+		.pipe(gzip({ gzipOptions: { level: 9 }, append: true }))
+		.pipe(gulp.dest('public/assets/js'))
+});
+
+gulp.task('gzip-fa-css', function() {
+	return gulp.src('public/assets/font-awesome/css/font-awesome.min.css')
+		.pipe(gzip({ gzipOptions: { level: 9 }, append: true }))
+		.pipe(gulp.dest('public/assets/font-awesome/css'))
+});
+
+gulp.task('gzip-fa-fonts', function() {
+	return gulp.src('public/assets/font-awesome/fonts/*')
+		.pipe(gzip({ gzipOptions: { level: 9 }, append: true }))
+		.pipe(gulp.dest('public/assets/font-awesome/fonts'))
+});
+
+gulp.task('default', [
+	'gzip-css',
+	'gzip-js',
+	'gzip-fa-css',
+	'gzip-fa-fonts'
+]);
