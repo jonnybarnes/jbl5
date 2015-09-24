6f2fb7a9abcfd829bc136ade34e307fb1bbe743d
Jonny Barnes <jonny@jonnybarnes.uk>
2015-09-24 17:27:40 +0100

    Add piwik code back, only in prod though

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -39,5 +39,24 @@
   @section('scripts')
   <!--scripts go here when needed-->
   @show
+
+  {{-- The piwik code that should only be shown in production --}}
+  @if (App::environment() == 'production')
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="https://piwik.jonnybarnes.uk/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 1]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="https://piwik.jonnybarnes.uk/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
+  @endif
 </body>
 </html>
