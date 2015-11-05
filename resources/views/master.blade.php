4701e1bfe2d283a20d1b878545c1e23c999b82a4
Jonny Barnes <jonny@jonnybarnes.uk>
2015-11-05 21:37:36 +0000

    Trying to do piwik better

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -40,21 +40,21 @@
   @show
 
   {{-- The piwik code that should only be shown in production --}}
-  @if (App::environment() == 'production')
+  @if (env('PIWIK_URL') !== null)
   <!-- Piwik -->
   <script type="text/javascript">
     var _paq = _paq || [];
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-      var u="https://piwik.jonnybarnes.uk/";
+      var u="{{ env('PIWIK_URL') }}";
       _paq.push(['setTrackerUrl', u+'piwik.php']);
       _paq.push(['setSiteId', 1]);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
       g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
     })();
   </script>
-  <noscript><p><img src="https://piwik.jonnybarnes.uk/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+  <noscript><p><img src="{{ env('PIWIK_URL') }}/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
   <!-- End Piwik Code -->
   @endif
 </body>
