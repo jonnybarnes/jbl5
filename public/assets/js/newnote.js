8094a4ed8d5cc6d1808b364cb295c55caf055ee2
Jonny Barnes <jonny@jonnybarnes.uk>
2015-11-23 20:29:54 +0000

    Micropub new place javascript should be working

--- public/assets/js/newnote.js
+++ public/assets/js/newnote.js
@@ -1,6 +1,8 @@
 if ('geolocation' in navigator) {
     var button = document.querySelector('#locate');
     if (button.addEventListener) {
+        //if we have javascript, event listeners and geolocation, make the locate
+        //button clickable and add event
         button.disabled = false;
         button.addEventListener('click', getLocation);
     }
@@ -8,11 +10,13 @@ if ('geolocation' in navigator) {
 
 function getLocation() {
     navigator.geolocation.getCurrentPosition(function (position) {
+        //the locate button has been clicked so add the places/map
         addPlaces(position.coords.latitude, position.coords.longitude);
     });
 }
 
 function addPlaces(latitude, longitude) {
+    //get the nearby places
     fetch('/places/near/' + latitude + '/' + longitude, {
         method: 'get'
     }).then(function (response) {
@@ -27,8 +31,10 @@ function addPlaces(latitude, longitude) {
                 var slug = j[i].slug;
                 places.push([name, slug, latlng[0], latlng[1]]);
             }
+            //add a map with the nearby places
             addMap(latitude, longitude, places);
         } else {
+            //add a map with just current location
             addMap(latitude, longitude);
         }
     }).catch(function (err) {
@@ -44,6 +50,7 @@ function addMap(latitude, longitude, places) {
     var form = button.parentNode;
     var div = document.createElement('div');
     div.setAttribute('id', 'map');
+    //add the map div
     form.appendChild(div);
     L.mapbox.accessToken = 'pk.eyJ1Ijoiam9ubnliYXJuZXMiLCJhIjoiVlpndW1EYyJ9.aP9fxAqLKh7lj0LpFh5k1w';
     var map = L.mapbox.map('map', 'jonnybarnes.gnoihnim')
@@ -51,9 +58,22 @@ function addMap(latitude, longitude, places) {
         .addLayer(L.mapbox.tileLayer('jonnybarnes.gnoihnim', {
             detectRetina: true,
         }));
+    //add a marker for the current location
     var marker = L.marker([latitude, longitude], {
         draggable: true,
     }).addTo(map);
+    //when the location marker is dragged, if the new place form elements exist
+    //update the lat/lng values
+    marker.on('dragend', function () {
+        var placeFormLatitude = document.querySelector('#place-latitude');
+        if (placeFormLatitude !== null) {
+            placeFormLatitude.value = getLatitudeFromMapboxMarker(marker.getLatLng());
+        }
+        var placeFormLongitude = document.querySelector('#place-longitude');
+        if (placeFormLongitude !== null) {
+            placeFormLongitude.value = getLongitudeFromMapboxMarker(marker.getLatLng());
+        }
+    });
     if (places !== null) {
         //create the <select> element and give it a no location default
         var selectEl = document.createElement('select');
@@ -99,15 +119,130 @@ function addMap(latitude, longitude, places) {
             }
         });
     } else {
+        //indicate there are now nearby places
         var noPlacesText = document.createTextNode('There are no nearby places.');
         var noPlacesPTag = document.createElement('p');
         noPlacesPTag.appendChild(noPlacesText);
         form.insertBefore(noPlacesPTag, div);
     }
+    //add a button to add a new place
     var newLocButton = document.createElement('button');
+    newLocButton.setAttribute('type', 'button');
+    newLocButton.setAttribute('id', 'create-new-place');
     newLocButton.appendChild(document.createTextNode('Create New Place?'));
-    var url = window.location.href;
-    var admin = /admin/.test(url);
+    //the event listener
+    newLocButton.addEventListener('click', function() {
+        //add the form elements
+        var nameLabel = document.createElement('label');
+        nameLabel.setAttribute('for', 'place-name');
+        nameLabel.classList.add('place-label')
+        nameLabel.appendChild(document.createTextNode('Place Name:'));
+        var nameEl = document.createElement('input');
+        nameEl.setAttribute('placeholder', 'Name');
+        nameEl.setAttribute('name', 'place-name');
+        nameEl.setAttribute('id', 'place-name');
+        nameEl.setAttribute('type', 'text');
+        var descLabel = document.createElement('label');
+        descLabel.setAttribute('for', 'place-description');
+        descLabel.classList.add('place-label');
+        descLabel.appendChild(document.createTextNode('Place Description:'));
+        var descEl = document.createElement('input');
+        descEl.setAttribute('placeholder', 'Description');
+        descEl.setAttribute('name', 'place-description');
+        descEl.setAttribute('id', 'place-description');
+        descEl.setAttribute('type', 'text');
+        var latLabel = document.createElement('label');
+        latLabel.setAttribute('for', 'place-latitude');
+        latLabel.classList.add('place-label');
+        latLabel.appendChild(document.createTextNode('Place Latitude:'));
+        var latEl = document.createElement('input');
+        latEl.setAttribute('name', 'place-latitude');
+        latEl.setAttribute('id', 'place-latitude');
+        latEl.setAttribute('type', 'text');
+        latEl.value = getLatitudeFromMapboxMarker(marker.getLatLng());
+        var lonLabel = document.createElement('label');
+        lonLabel.setAttribute('for', 'place-longitude');
+        lonLabel.classList.add('place-label');
+        lonLabel.appendChild(document.createTextNode('Place Longitude:'));
+        var lonEl = document.createElement('input');
+        lonEl.setAttribute('name', 'place-longitude');
+        lonEl.setAttribute('id', 'place-longitude');
+        lonEl.setAttribute('type', 'text');
+        lonEl.value = getLongitudeFromMapboxMarker(marker.getLatLng());
+        var placeSubmit = document.createElement('button');
+        placeSubmit.setAttribute('id', 'place-submit');
+        placeSubmit.setAttribute('value', 'Submit New Place');
+        placeSubmit.setAttribute('name', 'place-submit');
+        placeSubmit.setAttribute('type', 'button');
+        placeSubmit.appendChild(document.createTextNode('Submit New Place'));
+        form.appendChild(nameLabel);
+        form.appendChild(nameEl);
+        form.appendChild(descLabel);
+        form.appendChild(descEl);
+        form.appendChild(latLabel);
+        form.appendChild(latEl);
+        form.appendChild(lonLabel);
+        form.appendChild(lonEl);
+        form.appendChild(placeSubmit);
+        //the event listener for the new place form
+        placeSubmit.addEventListener('click', function () {
+            //create the form data to send
+            var formData = new FormData();
+            formData.append('place-name', document.querySelector('#place-name').value);
+            formData.append('place-description', document.querySelector('#place-description').value);
+            formData.append('place-latitude', document.querySelector('#place-latitude').value);
+            formData.append('place-longitude', document.querySelector('#place-longitude').value);
+            //post the new place
+            fetch('/places/new', {
+                //send cookies with the request
+                credentials: 'same-origin',
+                method: 'post',
+                body: formData
+            }).then(function (placeResponse) {
+                return placeResponse.json();
+            }).then(function (placeJson) {
+                //remove un-needed form elements
+                form.removeChild(document.querySelector('#place-name'));
+                form.removeChild(document.querySelector('#place-description'));
+                form.removeChild(document.querySelector('#place-latitude'));
+                form.removeChild(document.querySelector('#place-longitude'));
+                var labels = document.querySelectorAll('.place-label');
+                for (var label of labels) {
+                    form.removeChild(label);
+                }
+                form.removeChild(document.querySelector('#place-submit'));
+                form.removeChild(document.querySelector('#create-new-place'));
+                //remove location marker
+                map.removeLayer(marker);
+                //add place marker
+                var newOption = document.createElement('option');
+                newOption.setAttribute('value', placeJson['slug']);
+                newOption.appendChild(document.createTextNode(placeJson['name']));
+                newOption.dataset.latitude = placeJson['latitude'];
+                newOption.dataset.longitude = placeJson['longitude'];
+                selectEl.appendChild(newOption);
+                var newPlaceMarker = L.marker([placeJson['latitude'], placeJson['longitude']], {
+                    icon: L.mapbox.marker.icon({
+                        'marker-size': 'large',
+                        'marker-symbol': 'building',
+                        'marker-color': '#fa0'
+                    })
+                }).addTo(map);
+                var newName = 'Name: ' + placeJson['name'];
+                newPlaceMarker.bindPopup(newName, {
+                    closeButton: true
+                });
+                newPlaceMarker.on('click', function (e) {
+                    map.panTo([placeJson['latitude'], placeJson['longitude']]);
+                    selectPlace(placeJson['slug']);
+                });
+                //make selected
+                selectPlace(placeJson['slug']);
+            }).catch(function (placeError) {
+                console.log(placeError);
+            });
+        })
+    });
     form.insertBefore(newLocButton, div);
 }
 
@@ -121,4 +256,18 @@ function parseLocation(point) {
 
 function selectPlace(slug) {
     document.querySelector('select [value=' + slug + ']').selected = true;
+}
+
+function getLatitudeFromMapboxMarker(latlng) {
+    var resultArray = /\((.*)\)/.exec(latlng);
+    var location = resultArray[1].split(' ');
+
+    return location[0].replace(',', '');
+}
+
+function getLongitudeFromMapboxMarker(latlng) {
+    var resultArray = /\((.*)\)/.exec(latlng);
+    var location = resultArray[1].split(' ');
+
+    return location[1];
 }
