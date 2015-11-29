cbdf7ec28bda5024c7d330e5eb633bf9bc268781
Jonny Barnes <jonny@jonnybarnes.uk>
2015-11-29 17:41:06 +0000

    Don’t use Twitter’s widget.js to embed tweets

--- resources/views/singlepost.blade.php
+++ resources/views/singlepost.blade.php
@@ -24,23 +24,6 @@
   twemoji.parse(document.body);
 </script>
 <script src="{{ elixir('assets/js/prism.js') }}"></script>
-<script>window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
-
-  return t;
-}(document, "script", "twitter-wjs"));
-</script>
 
 <link rel="stylesheet" href="{{ elixir('assets/css/prism.css') }}">
 @stop
