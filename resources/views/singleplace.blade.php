b157bb9a83862e4dcd872db335e96c7c0610f711
Jonny Barnes <jonny@jonnybarnes.uk>
2015-12-06 19:07:52 +0000

    Accidentally repeated p-latitude, wanted p-lomgitude

--- resources/views/singleplace.blade.php
+++ resources/views/singleplace.blade.php
@@ -9,7 +9,7 @@
   <h1 class="p-name">{{ $place->name }}</h1>
   <p class="p-note">{{ $place->description or 'No description'}}</p>
   <div class="map" data-latitude="{{ $place->latitude }}" data-longitude="{{ $place->longitude }}"></div>
-  <p class="latlnginfo">Latitude: <span class="p-latitude">{{ $place->latitude }}</span>, longitude: <span class="p-latitude">{{ $place->longitude }}</span></p>
+  <p class="latlnginfo">Latitude: <span class="p-latitude">{{ $place->latitude }}</span>, longitude: <span class="p-longitude">{{ $place->longitude }}</span></p>
 </div>
 @stop
 
